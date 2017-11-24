317cadb4ffdbdded5b797dcecee913bd5d564b03
Ricardo Gladwell <ricardo.gladwell@digital.hmrc.gov.uk>
2017-11-24 11:21:48 +0000

    [MTDSA-1234][RG] Add VAT declaration

---
+++ func/uk/gov/hmrc/vatapi/resources/ValueAddedTaxReturnsSpec.scala
@@ -0,0 +1,54 @@
+package uk.gov.hmrc.vatapi.resources
+
+import uk.gov.hmrc.support.BaseFunctionalSpec
+import play.api.libs.json.Json
+
+class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
+
+  "VAT returns" should {
+
+    "should allow users to submit VAT returns" in {
+      given()
+        .des().vatReturns.expectVatReturnFor(vrn)
+        .when()
+        .post(s"/$vrn/return", Some(Json.parse("""{
+          "periodKey": "#001",
+          "vatDueSales": 100.25,
+          "vatDueAcquisitions": 100.25,
+          "totalVatDue": 200.50,
+          "vatReclaimedCurrPeriod": 100.25,
+          "netVatDue": 100.25,
+          "totalValueSalesExVAT": 100,
+          "totalValuePurchasesExVAT": 100,
+          "totalValueGoodsSuppliedExVAT": 100,
+          "totalAcquisitionsExVAT": 100,
+          "finalised": true
+        }""")))
+        .thenAssertThat()
+        .statusIs(201)
+    }
+
+    "should not allow users to submit undeclared VAT returns" in {
+      given()
+        .des().vatReturns.expectVatReturnFor(vrn)
+        .when()
+        .post(s"/$vrn/return", Some(Json.parse("""{
+          "periodKey": "#001",
+          "vatDueSales": 100.25,
+          "vatDueAcquisitions": 100.25,
+          "totalVatDue": 200.50,
+          "vatReclaimedCurrPeriod": 100.25,
+          "netVatDue": 100.25,
+          "totalValueSalesExVAT": 100,
+          "totalValuePurchasesExVAT": 100,
+          "totalValueGoodsSuppliedExVAT": 100,
+          "totalAcquisitionsExVAT": 100,
+          "finalised": false
+        }""")))
+        .thenAssertThat()
+        .statusIs(403)
+    }
+
+  }
+
+}
