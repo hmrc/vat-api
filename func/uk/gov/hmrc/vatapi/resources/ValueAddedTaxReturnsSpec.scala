760460688ac615b00d038c63e420c3df149c1c72
Krzysztof Hycnar <krzysztof.hycnar@digital.hmrc.gov.uk>
2018-01-24 16:05:32 +0000

    MTDSA-1253 - Submit VAT returns - minor refactoring of tests

--- func/uk/gov/hmrc/vatapi/resources/ValueAddedTaxReturnsSpec.scala
+++ func/uk/gov/hmrc/vatapi/resources/ValueAddedTaxReturnsSpec.scala
@@ -5,14 +5,7 @@ import uk.gov.hmrc.support.BaseFunctionalSpec
 
 class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
 
-  "VAT returns submission" should {
-
-    "allow users to submit VAT returns" in {
-      given()
-        .des().vatReturns.expectVatReturnSubmissionFor(vrn)
-        .when()
-        .post(s"/$vrn/returns", Some(Json.parse(
-          """{
+  private def body(finalised: Boolean = true) =  s"""{
           "periodKey": "#001",
           "vatDueSales": 50.00,
           "vatDueAcquisitions": 100.30,
@@ -23,8 +16,16 @@ class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
           "totalValuePurchasesExVAT": 200.00,
           "totalValueGoodsSuppliedExVAT": 100.00,
           "totalAcquisitionsExVAT": 540.00,
-          "finalised": true
-        }""")))
+          "finalised": $finalised
+        }"""
+
+  "VAT returns submission" should {
+
+    "allow users to submit VAT returns" in {
+      given()
+        .des().vatReturns.expectVatReturnSubmissionFor(vrn)
+        .when()
+        .post(s"/$vrn/returns", Some(Json.parse(body())))
         .thenAssertThat()
         .statusIs(201)
     }
@@ -33,20 +34,7 @@ class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
       given()
         .des().vatReturns.expectVatReturnSubmissionFor(vrn)
         .when()
-        .post(s"/$vrn/returns", Some(Json.parse(
-          """{
-          "periodKey": "#001",
-          "vatDueSales": 50.00,
-          "vatDueAcquisitions": 100.30,
-          "totalVatDue": 150.30,
-          "vatReclaimedCurrPeriod": 40.00,
-          "netVatDue": 110.30,
-          "totalValueSalesExVAT": 1000,
-          "totalValuePurchasesExVAT": 200.00,
-          "totalValueGoodsSuppliedExVAT": 100.00,
-          "totalAcquisitionsExVAT": 540.00,
-          "finalised": false
-        }""")))
+        .post(s"/$vrn/returns", Some(Json.parse(body(false))))
         .thenAssertThat()
         .statusIs(403)
         .bodyHasPath("\\errors(0)\\code", "NOT_FINALISED")
@@ -57,20 +45,7 @@ class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
       given()
         .des().vatReturns.expectVatReturnToFail(vrn, "INVALID_PERIODKEY")
         .when()
-        .post(s"/$vrn/returns", Some(Json.parse(
-          """{
-          "periodKey": "#234",
-          "vatDueSales": 50.00,
-          "vatDueAcquisitions": 100.30,
-          "totalVatDue": 150.30,
-          "vatReclaimedCurrPeriod": 40.00,
-          "netVatDue": 110.30,
-          "totalValueSalesExVAT": 1000,
-          "totalValuePurchasesExVAT": 200.00,
-          "totalValueGoodsSuppliedExVAT": 100.00,
-          "totalAcquisitionsExVAT": 540.00,
-          "finalised": true
-        }""")))
+        .post(s"/$vrn/returns", Some(Json.parse(body())))
         .thenAssertThat()
         .statusIs(400)
         .bodyHasPath("\\code", "INVALID_KEY_PERIOD")
@@ -80,20 +55,7 @@ class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
       given()
         .des().vatReturns.expectVatReturnToFail(vrn, "INVALID_ARN")
         .when()
-        .post(s"/$vrn/returns", Some(Json.parse(
-          """{
-          "periodKey": "#234",
-          "vatDueSales": 50.00,
-          "vatDueAcquisitions": 100.30,
-          "totalVatDue": 150.30,
-          "vatReclaimedCurrPeriod": 40.00,
-          "netVatDue": 110.30,
-          "totalValueSalesExVAT": 1000,
-          "totalValuePurchasesExVAT": 200.00,
-          "totalValueGoodsSuppliedExVAT": 100.00,
-          "totalAcquisitionsExVAT": 540.00,
-          "finalised": true
-        }""")))
+        .post(s"/$vrn/returns", Some(Json.parse(body())))
         .thenAssertThat()
         .statusIs(400)
         .bodyHasPath("\\code", "ARN_INVALID")
@@ -103,20 +65,7 @@ class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
       given()
         .des().vatReturns.expectVatReturnToFail(vrn, "INVALID_VRN")
         .when()
-        .post(s"/$vrn/returns", Some(Json.parse(
-          """{
-          "periodKey": "#234",
-          "vatDueSales": 50.00,
-          "vatDueAcquisitions": 100.30,
-          "totalVatDue": 150.30,
-          "vatReclaimedCurrPeriod": 40.00,
-          "netVatDue": 110.30,
-          "totalValueSalesExVAT": 1000,
-          "totalValuePurchasesExVAT": 200.00,
-          "totalValueGoodsSuppliedExVAT": 100.00,
-          "totalAcquisitionsExVAT": 540.00,
-          "finalised": true
-        }""")))
+        .post(s"/$vrn/returns", Some(Json.parse(body())))
         .thenAssertThat()
         .statusIs(400)
         .bodyHasPath("\\code", "VRN_INVALID")
@@ -126,20 +75,7 @@ class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
       given()
         .des().vatReturns.expectVatReturnToFail(vrn, "INVALID_PAYLOAD")
         .when()
-        .post(s"/$vrn/returns", Some(Json.parse(
-          """{
-          "periodKey": "#234",
-          "vatDueSales": 50.00,
-          "vatDueAcquisitions": 100.30,
-          "totalVatDue": 150.30,
-          "vatReclaimedCurrPeriod": 40.00,
-          "netVatDue": 110.30,
-          "totalValueSalesExVAT": 1000,
-          "totalValuePurchasesExVAT": 200.00,
-          "totalValueGoodsSuppliedExVAT": 100.00,
-          "totalAcquisitionsExVAT": 540.00,
-          "finalised": true
-        }""")))
+        .post(s"/$vrn/returns", Some(Json.parse(body())))
         .thenAssertThat()
         .statusIs(400)
         .bodyHasPath("\\code", "INVALID_REQUEST")
@@ -149,20 +85,7 @@ class ValueAddedTaxReturnsSpec extends BaseFunctionalSpec {
       given()
         .des().vatReturns.expectVatReturnToFail(vrn, "DUPLICATE_SUBMISSION")
         .when()
-        .post(s"/$vrn/returns", Some(Json.parse(
-          """{
-          "periodKey": "#234",
-          "vatDueSales": 50.00,
-          "vatDueAcquisitions": 100.30,
-          "totalVatDue": 150.30,
-          "vatReclaimedCurrPeriod": 40.00,
-          "netVatDue": 110.30,
-          "totalValueSalesExVAT": 1000,
-          "totalValuePurchasesExVAT": 200.00,
-          "totalValueGoodsSuppliedExVAT": 100.00,
-          "totalAcquisitionsExVAT": 540.00,
-          "finalised": true
-        }""")))
+        .post(s"/$vrn/returns", Some(Json.parse(body())))
         .thenAssertThat()
         .statusIs(403)
         .bodyHasPath("\\errors(0)\\code", "DUPLICATE_SUBMISSION")
