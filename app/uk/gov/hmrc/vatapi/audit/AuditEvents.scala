f46b177adeac1a9535ee91ef4e238d368eb56bb9
Jonathan Leather <jonathan.leather@digital.hmrc.gov.uk>
2018-11-09 09:30:12 +0000

    MTDSA-2512 - Added Retrieve VAT Liabilities Audit (#332)

--- app/uk/gov/hmrc/vatapi/audit/AuditEvents.scala
+++ app/uk/gov/hmrc/vatapi/audit/AuditEvents.scala
@@ -38,7 +38,6 @@ object AuditEvents {
   def submitVatReturn(correlationId: String, userType: String, nrSubmissionId: Option[String], arn: Option[String]): AuditEvent[Map[String, String]] = {
 
     val nrSubmissionIdMap: Map[String, String] = nrSubmissionId.fold(Map.empty[String, String])(id => Map("nrSubmissionId" -> id))
-    val arnMap: Map[String, String] = arn.fold(Map.empty[String, String])(arn => Map("agentReferenceNumber" -> arn))
 
     AuditEvent(
       auditType = "submitVatReturn",
@@ -67,6 +66,18 @@ object AuditEvents {
     AuditEvent(
       auditType = "retrieveVatReturns",
       transactionName = "retrieve-vat-returns",
+      detail = Map(
+        "X-CorrelationId" -> correlationId,
+        "userType" -> userType
+      ) ++ agentRef(arn)
+    )
+  }
+
+  def retrieveVatLiabilitiesAudit(correlationId: String, userType:String, arn: Option[String]): AuditEvent[Map[String, String]] = {
+
+    AuditEvent(
+      auditType = "retrieveVatLiabilities",
+      transactionName = "retrieve-vat-liabilities",
       detail = Map(
         "X-CorrelationId" -> correlationId,
         "userType" -> userType
