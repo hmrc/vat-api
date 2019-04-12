90d3ce57978af198371deac99ff4f13d85129a87
Mark Kelly <21219364+MarkAKelly@users.noreply.github.com>
2019-04-12 16:55:06 +0100

    [MTDSA-3317] Temporarily remove call to NRS (#571)

--- app/uk/gov/hmrc/vatapi/services/NRSService.scala
+++ app/uk/gov/hmrc/vatapi/services/NRSService.scala
@@ -27,6 +27,7 @@ import play.api.libs.json.Json
 import uk.gov.hmrc.domain.Vrn
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.vatapi.connectors.NRSConnector
+import uk.gov.hmrc.vatapi.httpparsers.EmptyNrsData
 import uk.gov.hmrc.vatapi.httpparsers.NrsSubmissionHttpParser.NrsSubmissionOutcome
 import uk.gov.hmrc.vatapi.models.VatReturnDeclaration
 import uk.gov.hmrc.vatapi.resources.AuthRequest
@@ -42,7 +43,8 @@ class NRSService @Inject()(
 
   def submit(vrn: Vrn, payload: VatReturnDeclaration)(implicit hc: HeaderCarrier, ec: ExecutionContext, request: AuthRequest[_]): Future[NrsSubmissionOutcome] = {
     logger.debug(s"[NRSService][submit] - Submitting payload to NRS")
-    nrsConnector.submit(vrn, convertToNrsSubmission(vrn, payload))
+//    nrsConnector.submit(vrn, convertToNrsSubmission(vrn, payload))
+    Future(Right(EmptyNrsData))
   }
 
   private def convertToNrsSubmission(vrn: Vrn, payload: VatReturnDeclaration)(implicit request: AuthRequest[_]): NRSSubmission = {
