e0222eeac22576588bbcd379234fe04e7878fcaa
jeremystone <6453521+jeremystone@users.noreply.github.com>
2019-05-24 08:54:30 +0100

    MTDSA-3528 - Add missing debug logging for success cases (retrieve return and retrieve obligations)

--- app/uk/gov/hmrc/vatapi/resources/VatReturnsResource.scala
+++ app/uk/gov/hmrc/vatapi/resources/VatReturnsResource.scala
@@ -124,6 +124,7 @@ class VatReturnsResource @Inject()(
           val result = response.filter {
             case OK => response.vatReturnOrError match {
               case Right(vatReturn) =>
+                logger.debug(s"[VatReturnsResource] [retrieveVatReturns] Successfully retrieved Vat Return from DES")
                 VatResult.Success(OK, vatReturn)
               case Left(error) =>
                 logger.error(s"[VatReturnsResource] [retrieveVatReturns] Json format from DES doesn't match the VatReturn model: ${error.msg}")
