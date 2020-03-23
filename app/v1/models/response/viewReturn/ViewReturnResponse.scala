d91ebbce6b13282a56645fe3d10531f4a7fc0e98
simon <55548012+simon1552@users.noreply.github.com>
2020-03-23 14:05:28 +0000

    added response model

---
+++ app/v1/models/response/viewReturn/ViewReturnResponse.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.response.viewReturn
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Json, OWrites, Reads}
+
+case class ViewReturnResponse(periodKey: String,
+                              vatDueSales: BigDecimal,
+                              vatDueAcquisitions: BigDecimal,
+                              totalVatDue: BigDecimal,
+                              vatReclaimedCurrPeriod: BigDecimal,
+                              vatDueNet: BigDecimal,
+                              totalValueSalesExVAT: BigDecimal,
+                              totalValuePurchasesExVAT: BigDecimal,
+                              totalValueGoodsSuppliedExVAT: BigDecimal,
+                              totalAcquisitionsExVAT: BigDecimal)
+object ViewReturnResponse {
+
+  implicit val writes: OWrites[ViewReturnResponse] = Json.writes[ViewReturnResponse]
+  implicit val reads: Reads[ViewReturnResponse] = (
+    (JsPath \ "periodKey").read[String] and
+      (JsPath \ "vatDueSales").read[BigDecimal] and
+      (JsPath \ "vatDueAcquisitions").read[BigDecimal] and
+      (JsPath \ "vatDueTotal").read[BigDecimal] and
+      (JsPath \ "vatReclaimedCurrPeriod"). read[BigDecimal] and
+      (JsPath \ "vatDueNet").read[BigDecimal] and
+      (JsPath \ "totalValueSalesExVAT").read[BigDecimal] and
+      (JsPath \ "totalValuePurchasesExVAT").read[BigDecimal] and
+      (JsPath \ "totalValueGoodsSuppliedExVAT").read[BigDecimal] and
+      (JsPath \ "totalAllAcquisitionsExVAT").read[BigDecimal]
+    )(ViewReturnResponse.apply _)
+
+}
---
+++ test/v1/models/response/ViewReturnResponseSpec.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.response
+
+import support.UnitSpec
+import play.api.libs.json.{JsValue, Json}
+import v1.models.response.viewReturn.ViewReturnResponse
+
+class ViewReturnResponseSpec extends UnitSpec {
+
+  val desViewReturn: JsValue = Json.parse(
+    """
+      |{
+      |    "periodKey": "A001",
+      |    "vatDueSales": 2119210210.23,
+      |    "vatDueAcquisitions": -9876543210912.87,
+      |    "vatDueTotal": 929201123.23,
+      |    "vatReclaimedCurrPeriod": -1234567890122.23,
+      |    "vatDueNet": 2345678901.12,
+      |    "totalValueSalesExVAT": 93732432923.00,
+      |    "totalValuePurchasesExVAT": 34853948.00,
+      |    "totalValueGoodsSuppliedExVAT": 82390428304.00,
+      |    "totalAllAcquisitionsExVAT": -204832482.00
+      |}
+      |""".stripMargin)
+
+  val mtdViewReturn: ViewReturnResponse = ViewReturnResponse(
+    periodKey = "A001",
+    vatDueSales = 2119210210.23,
+    vatDueAcquisitions = -9876543210912.87,
+    totalVatDue = 929201123.23,
+    vatReclaimedCurrPeriod = -1234567890122.23,
+    vatDueNet = 2345678901.12,
+    totalValueSalesExVAT = 93732432923.00,
+    totalValuePurchasesExVAT = 34853948.00,
+    totalValueGoodsSuppliedExVAT = 82390428304.00,
+    totalAcquisitionsExVAT = -204832482.00)gi
+
+  "ViewReturnResponse" when {
+    "read from valid JSON" should {
+      "produce the expected ViewReturnResponse object for a return" in {
+        desViewReturn.as[ViewReturnResponse] shouldBe mtdViewReturn
+      }
+    }
+  }
+}
