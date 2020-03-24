673885721710fd2774fe0c0b221e1b3a3b6f1ccc
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2020-03-24 14:58:51 +0000

    MTDSA-5083: Add connector for view VAT return endpoint.

---
+++ test/v1/connectors/ViewReturnConnectorSpec.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import mocks.MockAppConfig
+import uk.gov.hmrc.domain.Vrn
+import v1.connectors.httpparsers.ViewReturnConnector
+import v1.mocks.MockHttpClient
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.viewReturn.ViewRequest
+import v1.models.response.viewReturn.ViewReturnResponse
+
+import scala.concurrent.Future
+
+class ViewReturnConnectorSpec extends ConnectorSpec {
+
+  private val vrn: String = "123456789"
+  private val periodKey: String = "F034"
+
+  private val viewReturnRequest: ViewRequest =
+    ViewRequest(
+      vrn = Vrn(vrn),
+      periodKey = periodKey
+    )
+
+  private val viewReturnResponse: ViewReturnResponse =
+    ViewReturnResponse(
+      periodKey = "F034",
+      vatDueSales = 4567.23,
+      vatDueAcquisitions = -456675.5,
+      totalVatDue = 7756.65,
+      vatReclaimedCurrPeriod = -756822354.64,
+      vatDueNet = 8956743245.12,
+      totalValueSalesExVAT = 43556767890.00,
+      totalValuePurchasesExVAT = 34556790.00,
+      totalValueGoodsSuppliedExVAT = 34556.00,
+      totalAcquisitionsExVAT = -68978.00
+    )
+
+  class Test extends MockHttpClient with MockAppConfig {
+
+    val connector: ViewReturnConnector =
+      new ViewReturnConnector(
+        http = mockHttpClient,
+        appConfig = mockAppConfig
+      )
+
+    val desRequestHeaders: Seq[(String, String)] =
+      Seq(
+        "Environment" -> "des-environment",
+        "Authorization" -> s"Bearer des-token"
+      )
+
+    MockedAppConfig.desBaseUrl returns baseUrl
+    MockedAppConfig.desToken returns "des-token"
+    MockedAppConfig.desEnvironment returns "des-environment"
+  }
+
+  "ViewReturnConnector" when {
+    "viewing a VAT return" must {
+      "return a valid response" in new Test {
+        val outcome = Right(ResponseWrapper(correlationId, viewReturnResponse))
+
+        val queryParams: Seq[(String, String)] =
+          Seq(
+            "period-key" -> periodKey
+          )
+
+        MockedHttpClient
+          .get(
+            url = s"$baseUrl/vat/returns/vrn/$vrn",
+            queryParams = queryParams,
+            requiredHeaders = "Environment" -> "des-environment", "Authorization" -> s"Bearer des-token"
+          )
+          .returns(Future.successful(outcome))
+
+        await(connector.viewReturn(viewReturnRequest)) shouldBe outcome
+      }
+    }
+  }
+}
