850a7ae266f6c5c5edd23d6d3458ce02e4db5d77
Narendra Vijayarao <26090262+narendravijayarao@users.noreply.github.com>
2020-04-09 17:17:32 +0100

    Added Payments request parser and validator (#750)

---
+++ app/v1/controllers/requestParsers/PaymentsRequestParser.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers
+
+import javax.inject.Inject
+import uk.gov.hmrc.domain.Vrn
+import v1.controllers.requestParsers.validators.PaymentsValidator
+import v1.models.request.payments.{PaymentsRawData, PaymentsRequest}
+
+class PaymentsRequestParser @Inject()(val validator: PaymentsValidator)
+  extends RequestParser[PaymentsRawData, PaymentsRequest] {
+
+  override protected def requestFor(data: PaymentsRawData): PaymentsRequest = {
+    PaymentsRequest(Vrn(data.vrn), data.from.get, data.to.get)
+  }
+}
