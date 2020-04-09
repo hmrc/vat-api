ed0ec4e23966cb29c5ac35662844e51c4fee18b1
David Walters <32767569+david-walters-HMRC@users.noreply.github.com>
2020-04-09 10:56:52 +0100

    MTDSA-5133 added request parser and validation for retrieve liabilities (#745)
    * MTDSA-5133 added request parser and validation for retrieve liabilities

---
+++ app/v1/controllers/requestParsers/LiabilitiesRequestParser.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers
+
+import javax.inject.Inject
+import uk.gov.hmrc.domain.Vrn
+import v1.controllers.requestParsers.validators.LiabilitiesValidator
+import v1.models.request.liability.{LiabilityRawData, LiabilityRequest}
+
+class LiabilitiesRequestParser @Inject()(val validator: LiabilitiesValidator)
+  extends RequestParser[LiabilityRawData, LiabilityRequest] {
+
+  override protected def requestFor(data: LiabilityRawData): LiabilityRequest = {
+    LiabilityRequest(Vrn(data.vrn), data.from.get, data.to.get)
+  }
+}
