07364b2965d753a57d87314263367b707167e767
“ngangaNjiraini” <52919747+ngangaNjiraini@users.noreply.github.com>
2020-04-14 13:15:16 +0100

    MTDSA-5069 updated OBL controller

--- app/v1/controllers/ObligationsController.scala
+++ app/v1/controllers/ObligationsController.scala
@@ -2,22 +2,31 @@ package v1.controllers
 
 
 import javax.inject.Inject
-import play.api.mvc.{BaseController, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import uk.gov.hmrc.domain.Vrn
 import uk.gov.hmrc.vatapi.services.AuditService
-import utils.Logging
+import utils.{EndpointLogContext, Logging}
 import v1.controllers.requestParsers.ObligationsRequestParser
-import v1.services.ObligationsService
+import v1.services.{EnrolmentsAuthService, ObligationsService}
 
 import scala.concurrent.ExecutionContext
 
 @Singleton
 class ObligationsController @Inject()(val authService: EnrolmentsAuthService,
-                                      val lookupService: MtdIdLookupService,
                                       requestParser: ObligationsRequestParser,
                                       service: ObligationsService,
-                                      hateoasFactory: HateoasFactory,
                                       auditService: AuditService,
                                       cc: ControllerComponents)(implicit ec: ExecutionContext)
   extends AuthorisedController(cc) with BaseController with Logging {
 
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(
+      controllerName = "VatObligationsResource",
+      endpointName = "retrieveVatObligations"
+    )
+
+  def Obligations(vrn: String, from: Option[String], to: Option[String], status: Option[String]): Action[AnyContent] =
+    authorisedAction(vrn).async{ implicit request =>
+
+    }
 }
