ea667e5807deb0b2de1611afd122b812030cf3cb
@diksha-diksha <176032121+diksha-diksha@users.noreply.github.com>
2024-10-09 12:34:16 +0100

    DL-14575: VAT API-Removed scopes and refactored Unit and func test

--- app/definition/ApiDefinition.scala
+++ app/definition/ApiDefinition.scala
@@ -74,11 +74,7 @@ object APIDefinition {
 
 case class Scope(key: String, name: String, description: String)
 
-object Scope {
-  implicit val formatScope: OFormat[Scope] = Json.format[Scope]
-}
-
-case class Definition(scopes: Seq[Scope], api: APIDefinition)
+case class Definition(api: APIDefinition)
 
 object Definition {
   implicit val formatDefinition: OFormat[Definition] = Json.format[Definition]
